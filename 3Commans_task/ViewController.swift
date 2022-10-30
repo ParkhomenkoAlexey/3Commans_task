aa203e20324e21506aab22ccf849051caa8c62df
Alexey Parkhomenko <alexey@tap.pm>
2022-10-30 18:52:40 +0300

    Initial Commit

---
+++ 3Commans_task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3Commans_task
+//
+//  Created by Алексей on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
